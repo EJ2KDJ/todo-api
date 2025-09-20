4921f2cff38672df8eb78ab3fa73a2bc41943c3b
EJ2KDJ <65149114+EJ2KDJ@users.noreply.github.com>
2025-09-20 17:15:34 +0800

    db migration and fixed associations

--- server.js
+++ server.js
@@ -6,15 +6,17 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
+//Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 app.get('/', (req, res) => {
     res.send('Welcome to the Todo API');
 });
 
+
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
-});
-
-app.use('/', (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Internal Server Error');
 });
